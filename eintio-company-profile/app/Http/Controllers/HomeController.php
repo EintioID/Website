7d8bfecce07ad115b0405bc22474c2ebdc0d2e64
EintioID <eintio.sosmed@gmail.com>
2026-09-13 09:35:59 +0700

    Update HomeController

--- eintio-company-profile/app/Http/Controllers/HomeController.php
+++ eintio-company-profile/app/Http/Controllers/HomeController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\Profile;
 use App\Models\CoreValue;
+use App\Models\Portfolio;
 
 class HomeController extends Controller
 {
@@ -13,15 +14,21 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('index');
+        $portfolios = Portfolio::with('category')
+            ->where('status', 'published')
+            ->latest()
+            ->take(3)
+            ->get();
+
+        return view('index', compact('portfolios'));
     }
 
     public function profile()
-{
-    $profile = Profile::first() ?? new Profile();
+    {
+        $profile = Profile::first() ?? new Profile();
 
-    $coreValues = CoreValue::orderBy('id')->get();
+        $coreValues = CoreValue::orderBy('id')->get();
 
-    return view('profile', compact('profile', 'coreValues'));
-}
+        return view('profile', compact('profile', 'coreValues'));
+    }
 }
